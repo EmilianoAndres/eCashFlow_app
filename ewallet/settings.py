e3fbf123b1f8eacaf008999c38b04964062ccb99
EmilianoAndres <emilianocraviotto@gmail.com>
2023-08-21 23:09:20 -0300

    commit

--- ewallet/settings.py
+++ ewallet/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306'
     },
-    'test': {
+    'test_db': {
         'ENGINE': 'mysql.connector.django',
         'NAME': 'test_db',
         'USER': 'root',
